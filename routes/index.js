f0e3c47b320af47c7d6a4cdb80f6466d883c6bd4
Erik Sutherland <erik.misu@gmail.com>
2015-03-06 22:26:01 -0800

    root page directs user to home server repo on git

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.json({'Home-Conrol': 'https://github.com/MrRacoon/home-control-server.git'});
 });
 
 module.exports = router;
