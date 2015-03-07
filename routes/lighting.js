52a63bb16472cae8506bcb272528ecad5a129438
Erik Sutherland <erik.misu@gmail.com>
2015-03-06 22:41:22 -0800

    New color endpoint, explicit color change by name

--- routes/lighting.js
+++ routes/lighting.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router  = express.Router();
 
+var colorMap = {
+    red        : 0x0000
+    , orange   : 0x2000
+    , yellow   : 0x3000
+    , green    : 0x5000
+    , cyan     : 0x8000
+    , blue     : 0x9000
+    , black    : 0xa000
+    , purple   : 0xc000
+    , pink     : 0xd000
+    , darkPink : 0xe000
+};
+
 /* GET home page. */
 router.get('/', function(req, res) {
     res.json('lighting Control');
@@ -30,4 +43,9 @@ router.get('/change/:hue/:sat/:lum/:whi/:fad', function(req, res) {
     res.json('Changing Lights');
 });
 
+router.get('/color/:col', function(req, res) {
+    var l = req.lifx;
+    l.lightsColour(colorMap[req.params.col],0xffff,0xffff,0x0dac,0x0513);
+    res.json('Changing to color ' + req.params.col);
+});
 module.exports = router;
